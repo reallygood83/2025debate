ec9ee12023906d6ca54b64460dc2fc207d5630a3
Your Name <your-email@example.com>
2025-04-23 14:27:59 +0900

    Fix: Remove React component code from API route file

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,10 +5,6 @@ import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
 import dbConnect from '../../../lib/mongoose';
 import User from '../../../models/User';
-import bcrypt from 'bcrypt';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { GetServerSideProps } from 'next';
-import { getServerSession } from 'next-auth/next';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -93,43 +89,4 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-export default NextAuth(authOptions);
-
-export default function AuthButtons() {
-  const { data: session } = useSession();
-  
-  if (session) {
-    return (
-      <button onClick={() => signOut()}>
-        로그아웃
-      </button>
-    );
-  }
-  
-  return (
-    <button onClick={() => signIn()}>
-      로그인
-    </button>
-  );
-}
-
-export default function ProtectedPage() {
-  return <h1>인증된 사용자만 볼 수 있는 페이지입니다</h1>;
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
-  
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth/signin',
-        permanent: false,
-      },
-    };
-  }
-  
-  return {
-    props: { session },
-  };
-}; 
+export default NextAuth(authOptions); 
